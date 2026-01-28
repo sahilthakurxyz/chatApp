245eed770c96bd89a6b8c0489c543e37f58e7423
sahilthakurxyz <sahilthakurxyz@gmail.com>
2026-01-29 02:03:57 +0530

    check

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const { v2: cloudinary } = require("cloudinary");
 const connectDB = require("./config/database.js");
 
 const port = process.env.PORT || 4000;
+console.log(process.env.PORT, "PORT");
 process.on("uncaughtException", (err) => {
   console.log(`Server is Shutting Down due to Unchaught Error,${err.message}`);
 
